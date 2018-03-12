6f1f47fbf04ce7a684fab7ed043829cff2b93280
hz1490919302 <1490919302@qq.com>
2018-03-12 21:15:19 +0800

    Finish alu

---
+++ Lab1/alu.v
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Module Name: lab1
+//////////////////////////////////////////////////////////////////////////////////
+module alu(x,y,aluop,result,equal);
+input[31:0]x;
+input[31:0]y;
+input[3:0]aluop;
+output reg [31:0]result;
+output reg equal;
+
+
+always@(x or y or aluop)
+begin
+    case(aluop)
+    0:result=x<<(y[4:0]);//左移
+    1:result=($signed(x))>>>(y[4:0]);//算术右移
+    2:result=x>>(y[4:0]);//逻辑右移
+    3:result=x*y;    //乘，cpu里不需要用到乘
+    4:result=x/y;    //除，cpu里不需要用到除
+    5:result=x+y;    //加法
+    6:result=x-y;    //减法
+    7:result=x&y;    //按位与
+    8:result=x|y;    //按位或
+    9:result=x^y;    //按位异或
+    10:result=~(x|y);//按位或非
+    11:result= ($signed(x)<$signed(y))?1:0;       //符号比较
+    12:result= ($unsigned(x)<$unsigned(y))?1:0;  //无符号比较
+    default:result=result;
+    endcase
+end
+
+always@(x or y or aluop)
+begin
+    equal=(x==y)?1:0;
+end
+    
+endmodule
