b6b2b909f602538bb506ffeb889535b4c973db78
HdmmY <2439846178@qq.com>
2018-03-15 09:56:21 +0800

    Fixed Display Mode

--- Lab1/DemultFrequency.v
+++ Lab1/DemultFrequency.v
@@ -3,7 +3,7 @@
  * Team: Blabla
  * Date: 3/12/2018
  */
-//@Sys_Clk is 100MHZ, @display_Freq is 1KHZ, @Fra_Freq is @Sys_clk(100MH) or 64Hz choosed by
+//@Sys_Clk is 100MHZ, @display_Freq is 1KHZ, @Fra_Freq is @Sys_Clk(100MH) or 64Hz choosed by
 //the @Fre_Choice(0 for the former, 1 for the latter).
 //@Cycles sums up all the cycles run durring the program, cleaned up by the @rst_n.
 //@display_Freq for displaying the LEDS   
@@ -16,26 +16,33 @@ module DemultFrequency (
     output reg display_Freq,
     output reg [15:0] Cycles   //total cycles run
 );
-    integer m=0, n=0, k=0;
+    reg [31:0] m;
+    reg [31:0] n;
+    reg [31:0] k;
     
     initial 
     begin
+        m = 32'd0;
+        n = 32'd0;
+        k = 32'd0;
         Fra_Freq = 0;
         display_Freq = 0;
         Cycles = 16'b0; 
     end
 
 
-    always @(rst_n or  Sys_Clk or Fre_Choice)
+    always @(posedge Sys_Clk)
     begin
         /*reset*/
         if(rst_n) begin
             Cycles = 16'b0;
-            m = 0;
-            n = 0;
-			k = 0;
-        end else if(n == 100000) begin
-            n = 0;
+            m = 32'd0;
+            n = 32'd0;
+			k = 32'd0;
+        end
+        
+        if(n == 32'd200000) begin
+            n = 32'd0;
             display_Freq = ~display_Freq;
         end else begin
             n = n+1;
@@ -44,9 +51,9 @@ module DemultFrequency (
         /*fra freq*/
         if(Fre_Choice)
         begin
-            if(m==1562500)
+            if(m == 32'd1562500)
             begin 
-                m = 0;
+                m = 32'd0;
                 Fra_Freq = ~Fra_Freq;
                 Cycles = Cycles+1;
             end
@@ -55,11 +62,11 @@ module DemultFrequency (
         end
         else
         begin
-			if(k==4)
+			if(k == 32'd4)
 			begin
             Fra_Freq = ~Fra_Freq;
             Cycles = Cycles+1;
-            k = 0;
+            k = 32'd0;
 			end
 			else
 				k = k+1;
